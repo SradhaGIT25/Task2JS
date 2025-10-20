4ee8a81d5181f08c9200ac2df2d079bc8e5d1db7
SradhaGIT25 <sradha.s@vonnue.com>
2025-10-21 01:08:30 +0530

    Update qn3.js
    updated

--- Task6 JS Challenge/Date Formatting/qn3.js
+++ Task6 JS Challenge/Date Formatting/qn3.js
@@ -1,7 +1,10 @@
-const date=new Date()
-console.log(
-   new Intl.DateTimeFormat( {
-    dateStyle: "long",
-    timeStyle: "short",
-  }).format(date),
-);
+const date=new Date();
+console.log(formatting(date));
+function formatting(date){
+const format=new Intl.DateTimeFormat("en-US", {
+    timeZone: "Asia/Kolkata",
+dateStyle:"long",
+timeStyle:"short",
+}).format(date);
+return format;
+}
