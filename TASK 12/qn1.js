a0e13309557b7ef1d879c0a523d0245d9c3b19ad
SradhaGIT25 <sradha.s@vonnue.com>
2025-10-27 21:56:59 +0530

    Delete TASK 12 directory

--- TASK 12/qn1.js
+++
@@ -1,6 +0,0 @@
-function calculateTotal(billAmount,taxPercentage){
-    let taxAmount=(taxPercentage/100)*billAmount;
-    let total=billAmount+taxAmount;
-    console.log(total)
-}
-calculateTotal(3000,15)
--- TASK 12/qn4.js
+++
@@ -1,7 +0,0 @@
-
-function combineObj(obj1,obj2){
-    let merged=Object.assign(obj1,obj2);
-    console.log(merged);
-}
-combineObj({ a: 1, b: 2 },{ b: 3, c: 4 }
-)
