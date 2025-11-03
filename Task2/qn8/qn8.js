b81d659276f936348e1dde9e58afed0609c449ff
SradhaGIT25 <sradha.s@vonnue.com>
2025-11-03 16:18:35 +0530

    Update qn8.js

--- Task2/qn8/qn8.js
+++ Task2/qn8/qn8.js
@@ -1,35 +1,91 @@
-// testCases=[{expected :  "programming" , input:"Hello world of programming"},{expected :  "jumps" , input: "The quick brown fox jumps over the lazy dog"} , {expected : "coding" , input:"I love coding"}];
+testCases=[{expected : 4 , input:[1, 2, 3, 5, 6]},
+  {expected :  12 , input: [10, 11, 13, 14, 15]} , 
+  {expected :  false , input: [10, 11, 13, 15]} ,
+  {expected :  false , input: "sradha"} ,
+  {expected :  false , input: true} ,
+  {expected :  false , input: false} ,
+  {expected :  false , input: null} ,
+  {expected :  false , input: undefined} ,
+  {expected :  false , input: []} ,
+  {expected :  9 , input: [10, 11, 12, 13, 14, 15]} ,
+  {expected :  13 , input: [15, 14, 12, 11]} ,
+  {expected :  false , input: ""} ,
+  {expected :  false , input: 0} ,
+  {expected :  12 , input: [10, 13, 11, 15, 14]} ,
+  {expected :  false , input: ["sds", "sdaS", "sqf", "wefe", "ggf"]} ,
+  {expected :  false , input: {}} ,
+  {expected :  9 , input: [10]} ,
+  {expected :  false , input: [10, "sds", 13, "qwd", 15]} ,
+  {expected : false , input:[{}]}];
 
-let numbers=[2, 3, 4, 5, 6];
-let number =[...numbers];
+test(testCases);
+function test(testCases) {
+    testCases.forEach((testCase, index) => {
+    const result = missing(testCase.input);
+
+    if (result === testCase.expected) {
+      console.log(`test ${index + 1} passed`);
+    } else {
+      console.log(`test ${index + 1} failed`);
+    }
+  });
+}
+
+function missing(numbers){
+
+  for (let i=0;i<numbers.length;i++){
+    if(typeof numbers[i] !== "number"){
+      return false;
+    }
+  }
+// console.log(num)
+if(!Array.isArray(numbers) || numbers.length===0 || numbers === null || numbers === undefined){
+  return false;
+}
+else{
+  let num=numbers.sort();
+  let number =[...num];
 let len=numbers.length;
 let first=numbers[0];
-console.log(first);
 let last=numbers[len-1];
-console.log(last);
 const a1=[];
 let found=0;
+let missed=0;
+let count=0;
 function createList(){
 let n=len;
 for(let i=first;i<=last;i++){
     a1.push(i);
 }
-console.log(a1);
 }
 createList();                                                                                                               
-
 for(let i=0;i<a1.length;i++){
     if(!numbers.includes(a1[i])){
         found=a1[i];
-        return found;
         console.log(`Element not found is, ${found}`);
+        missed=found;
+        console.log(found)
+        count++;  
     }
   }
-    if(number<0){
-      found=null;
+  
+    if(count>1){
+      return false;
+    }
+    if(found!==0){
+      return missed;
     }
           found=numbers[0]-1;
-          return found;
           console.log(`Element not found is, ${found}`);
+          missed=found;
+          count++;
+          return found;
+
     
+    
+    // return missed;
+}
+}
+
+
     
